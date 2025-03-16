1e62ac94853ffa357e007ec2955186b143e34964
Brandon Lee <119604447+brandonleehs@users.noreply.github.com>
2025-03-17 06:37:40 +0800

    fix: center login and signup form

--- lab3/src/LoginPage.jsx
+++ lab3/src/LoginPage.jsx
@@ -67,142 +67,145 @@ export default function LoginPage() {
           &copy; 2025 ParkSmart. All rights reserved.
         </footer>
       </section>
-      <form className="grid max-w-[400px] grow gap-5 self-center p-4 text-start min-[600px]:max-w-none">
-        <section className="grid gap-2">
-          <h1 className="text-3xl font-semibold">Login</h1>
-          <p className="text-gray-600">
-            Enter your credentials to access your account.
-          </p>
-        </section>
-        <main className="grid gap-4">
-          <div className="grid gap-1">
-            <label htmlFor="email">Email Address</label>
-            <div className="relative inline-flex items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="#9ca3af"
-                className="bi bi-envelope absolute left-2"
-                viewBox="0 0 16 16"
-              >
-                <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z" />
-              </svg>
-              <input
-                className="w-full rounded-md border border-gray-400 px-8 py-1 focus:outline-1 focus:outline-gray-600"
-                placeholder="Enter your email"
-                type="email"
-                id="email"
-                pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
-                size="30"
-                required
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
-              ></input>
-            </div>
-          </div>
-
-          <div className="grid gap-1">
-            <label htmlFor="password">Password</label>
-            <div className="relative inline-flex items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="#9ca3af"
-                className="bi bi-lock-fill absolute left-2"
-                viewBox="0 0 16 16"
-              >
-                <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2m3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2" />
-              </svg>
-              <input
-                className="w-full rounded-md border border-gray-400 px-8 py-1 focus:outline-1 focus:outline-gray-600"
-                placeholder="Create a password"
-                id="password"
-                type={passwordVisbility ? "text" : "password"}
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
-                required
-              ></input>
-              <button
-                className="absolute right-2 h-4 w-4"
-                type="button"
-                onMouseOver={() => {
-                  document
-                    .querySelector(".active")
-                    .setAttribute("fill", "#4b5563");
-                }}
-                onMouseOut={() => {
-                  document
-                    .querySelector(".active")
-                    .setAttribute("fill", "#9ca3af");
-                }}
-                onClick={() => {
-                  const eye = document.querySelector(".bi.bi-eye-slash");
-                  eye.classList.toggle("active");
-                  eye.classList.toggle("hidden");
-                  const eyeslash = document.querySelector(".bi.bi-eye");
-                  eyeslash.classList.toggle("active");
-                  eyeslash.classList.toggle("hidden");
-                  setPasswordVisibility(!passwordVisbility);
-                }}
-              >
+      <div className="flex grow items-center justify-center">
+        <form className="grid max-w-[400px] grow gap-5 self-center p-4 text-start min-[600px]:max-w-[600px]">
+          <section className="grid gap-2">
+            <h1 className="text-3xl font-semibold">Login</h1>
+            <p className="text-gray-600">
+              Enter your credentials to access your account.
+            </p>
+          </section>
+          <main className="grid gap-4">
+            <div className="grid gap-1">
+              <label htmlFor="email">Email Address</label>
+              <div className="relative inline-flex items-center">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
                   height="16"
                   fill="#9ca3af"
-                  className="bi bi-eye-slash active"
+                  className="bi bi-envelope absolute left-2"
                   viewBox="0 0 16 16"
                 >
-                  <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7 7 0 0 0-2.79.588l.77.771A6 6 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755q-.247.248-.517.486z" />
-                  <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829" />
-                  <path d="M3.35 5.47q-.27.24-.518.487A13 13 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7 7 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12z" />
+                  <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z" />
                 </svg>
+                <input
+                  className="w-full rounded-md border border-gray-400 px-8 py-1 focus:outline-1 focus:outline-gray-600"
+                  placeholder="Enter your email"
+                  type="email"
+                  id="email"
+                  pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
+                  size="30"
+                  required
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                ></input>
+              </div>
+            </div>
+            <div className="grid gap-1">
+              <label htmlFor="password">Password</label>
+              <div className="relative inline-flex items-center">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
                   height="16"
                   fill="#9ca3af"
-                  className="bi bi-eye hidden"
+                  className="bi bi-lock-fill absolute left-2"
                   viewBox="0 0 16 16"
                 >
-                  <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
-                  <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
+                  <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2m3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2" />
                 </svg>
-              </button>
+                <input
+                  className="w-full rounded-md border border-gray-400 px-8 py-1 focus:outline-1 focus:outline-gray-600"
+                  placeholder="Create a password"
+                  id="password"
+                  type={passwordVisbility ? "text" : "password"}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                  required
+                ></input>
+                <button
+                  className="absolute right-2 h-4 w-4"
+                  type="button"
+                  onMouseOver={() => {
+                    document
+                      .querySelector(".active")
+                      .setAttribute("fill", "#4b5563");
+                  }}
+                  onMouseOut={() => {
+                    document
+                      .querySelector(".active")
+                      .setAttribute("fill", "#9ca3af");
+                  }}
+                  onClick={() => {
+                    const eye = document.querySelector(".bi.bi-eye-slash");
+                    eye.classList.toggle("active");
+                    eye.classList.toggle("hidden");
+                    const eyeslash = document.querySelector(".bi.bi-eye");
+                    eyeslash.classList.toggle("active");
+                    eyeslash.classList.toggle("hidden");
+                    setPasswordVisibility(!passwordVisbility);
+                  }}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="#9ca3af"
+                    className="bi bi-eye-slash active"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7 7 0 0 0-2.79.588l.77.771A6 6 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755q-.247.248-.517.486z" />
+                    <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829" />
+                    <path d="M3.35 5.47q-.27.24-.518.487A13 13 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7 7 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12z" />
+                  </svg>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="#9ca3af"
+                    className="bi bi-eye hidden"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
+                    <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0" />
+                  </svg>
+                </button>
+              </div>
+              <small className="incorrect hidden text-red-600">
+                Credentials are incorrect. Please try again.
+              </small>
+              <small
+                className={"error text-red-600" + (error ? "" : " hidden")}
+              >
+                Please check that you have filled up all fields correctly.
+              </small>
             </div>
-            <small className="incorrect hidden text-red-600">
-              Credentials are incorrect. Please try again.
-            </small>
-            <small className={"error text-red-600" + (error ? "" : " hidden")}>
-              Please check that you have filled up all fields correctly.
-            </small>
-          </div>
-          <div className="flex gap-1.5">
-            <input type="checkbox" id="remember" name="remember" value="1" />
-            <label htmlFor="remember">Remember me</label>
-          </div>
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              signIn();
-            }}
-            type="submit"
-            className="rounded-md bg-blue-600 p-2 font-semibold text-neutral-100 shadow-md transition hover:bg-blue-700 active:bg-blue-800"
-          >
-            Login
-          </button>
-        </main>
-        <small>
-          Don't have an account?{" "}
-          <Link to="/signup" className="font-bold text-blue-600">
-            Sign up
-          </Link>
-        </small>
-      </form>
+            <div className="flex gap-1.5">
+              <input type="checkbox" id="remember" name="remember" value="1" />
+              <label htmlFor="remember">Remember me</label>
+            </div>
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                signIn();
+              }}
+              type="submit"
+              className="rounded-md bg-blue-600 p-2 font-semibold text-neutral-100 shadow-md transition hover:bg-blue-700 active:bg-blue-800"
+            >
+              Login
+            </button>
+          </main>
+          <small>
+            Don't have an account?{" "}
+            <Link to="/signup" className="font-bold text-blue-600">
+              Sign up
+            </Link>
+          </small>
+        </form>
+      </div>
     </div>
   );
 }

7039d3855c2e450d19f1ea8298326123df0d6610
Brandon Lee <119604447+brandonleehs@users.noreply.github.com>
2025-03-17 09:23:23 +0800

    feat: add loading

--- lab3/src/auth/AuthWrapper.jsx
+++ lab3/src/auth/AuthWrapper.jsx
@@ -1,15 +1,28 @@
-import { createContext, useEffect, useState } from 'react';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { createContext, useState, useEffect, useRef } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import LoadingPage from "../pages/LoadingPage";
 
 export const AuthContext = createContext();
 
 export function AuthWrapper({ children }) {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true); // Track loading state
 
-  const auth = getAuth();
-  onAuthStateChanged(auth, (user) => {
-    setUser(user);
-  });
+  useEffect(() => {
+    const auth = getAuth();
+
+    const unsubscribe = onAuthStateChanged(auth, (newUser) => {
+      console.log("user has changed!", user, newUser);
+      setUser(newUser);
+      setLoading(false); // Authentication check complete
+    });
+
+    return unsubscribe; // Cleanup on unmount
+  }, []);
+
+  if (loading) {
+    return <LoadingPage></LoadingPage>;
+  }
 
   return (
     <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>

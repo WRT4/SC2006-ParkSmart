b97c32aff9003fa117a4835cdf7ed6665e838912
Bradley1112 <129065978+Bradley1112@users.noreply.github.com>
2025-04-06 22:48:02 +0800

    Update LoadingPage.jsx

--- lab3/app/src/pages/LoadingPage.jsx
+++ lab3/app/src/pages/LoadingPage.jsx
@@ -2,10 +2,10 @@ import "../styles/LoadingPage.css";
 
 export default function LoadingPage() {
   return (
-    <main className="loading grid min-h-screen min-w-screen place-items-center">
+    <main className="loading grid min-h-screen min-w-screen place-items-center bg-white dark:bg-gray-900">
       <div className="flex flex-col items-center gap-5">
-        <div className="loader"></div>
-        <p>Checking sign-in status...</p>
+        <div className="loader dark:loader-dark"></div>
+        <p className="text-gray-800 dark:text-gray-200">Checking sign-in status...</p>
       </div>
     </main>
   );
